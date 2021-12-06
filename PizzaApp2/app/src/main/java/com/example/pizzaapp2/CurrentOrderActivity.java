2999b63f924cecc850d1d6525052a782dadadd4f
Kevin Cubillos <kevincubillos.KC@gmail.com>
2021-12-05 21:01:11 -0500

    Adding other screens

---
+++ PizzaApp2/app/src/main/java/com/example/pizzaapp2/CurrentOrderActivity.java
@@ -0,0 +1,6 @@
+package com.example.pizzaapp2;
+
+import android.app.Activity;
+
+public class CurrentOrderActivity extends Activity {
+}
---
+++ PizzaApp2/app/src/main/java/com/example/pizzaapp2/StoreOrdersActivity.java
@@ -0,0 +1,6 @@
+package com.example.pizzaapp2;
+
+import android.app.Activity;
+
+public class StoreOrdersActivity extends Activity {
+}
